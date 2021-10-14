5df7219ae10900e15f55e270a80f3e0ed798256e
Guilherme Rodriguero <gui.rodriguero@hotmail.com>
2021-10-14 01:31:07 -0300

    Ligações funcinoando

---
+++ twilio_files.py
@@ -0,0 +1,6 @@
+from twilio.rest import Client
+
+account_sid = "ACdc6d5314f523d60b9a88e75913f0167a"
+auth_token = "3d68a21be11cddfb030b77bf0794d89f"
+meu_numero = "+5511985728413" #Recebe ligacao
+numero_twilio = "+12183079279" #Faz ligacao
