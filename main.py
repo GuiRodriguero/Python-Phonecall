48d666ca3609217d87a7b041771f0e929244cffa
GuiRodriguero <50684900+GuiRodriguero@users.noreply.github.com>
2021-10-14 01:46:23 -0300

    Enviando mensagem
    Melhorar para criar um menu de escolha 1(liga) 2(sms)

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from twilio.rest import Client
 
 account_sid = account_sid
 auth_token = auth_token
-meu_numero = meu_numero #Recebe ligacao
-numero_twilio = numero_twilio #Faz ligacao
+meu_numero = meu_numero #Recebe ligacao/sms
+numero_twilio = numero_twilio #Faz ligacao/sms
 
 client = Client(account_sid, auth_token)
 
-mensagem = """
-    <Response>
-        <Say language="pt-BR">
-            Testando mensagem programada
-        </Say>
-    </Response>
-"""
+# mensagem = """
+#     <Response>
+#         <Say language="pt-BR">
+#             Testando mensagem programada
+#         </Say>
+#     </Response>
+# """
 
-ligacao = client.calls.create(to=meu_numero, from_=numero_twilio, twiml=mensagem)
-#mensage = client.messages.create()
+#ligacao = client.calls.create(to=meu_numero, from_=numero_twilio, twiml=mensagem)
+mensage = client.messages.create(to=meu_numero, from_=numero_twilio, body="Olá")
